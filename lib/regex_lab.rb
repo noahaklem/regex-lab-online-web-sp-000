14c8aa0cbf24acf4369992656e94c9f606fbca97
Noah  Klem <klem.noah@gmail.com>
2021-05-28 16:12:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]/i)
+    return true
+  else 
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/\b/i)
 end
 
 def words_five_letters_long(text)
